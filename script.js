a9801a324e37cb0cc75d130902bd0b84a1ba683f
Krischelle <krischelle_cadao@dlsu.edu.ph>
2024-04-12 15:51:59 +0800

    func init

--- script.js
+++ script.js
@@ -1,42 +1,47 @@
-import * as THREE from 'three';
+// import * as THREE from 'three';
+let camera, scene, renderer;
+const originalBoxSize = 3;  //Original width and height of box
 
-//Scene
-const scene = new THREE.Scene();
 
-//  Adding cube to the scene
-const geometry = new THREE.BoxGeometry(3, 1, 3);
-const material = new THREE.MeshLambertMaterial({color: 0xFB8E00});
-const mesh = new THREE.Mesh(geometry, material);
-mesh.position.set(0,0,0);
-scene.add(mesh);
+function init() {
+    //Scene
+    scene = new THREE.Scene();
 
-//  Setting Lights
-const ambientLight = new THREE.AmbientLight(0xFFFFFF, 0.6);
-scene.add(ambientLight);
+    //  Adding cube to the scene
+    const geometry = new THREE.BoxGeometry(3, 1, 3);
+    const material = new THREE.MeshLambertMaterial({ color: 0xFB8E00 });
+    const mesh = new THREE.Mesh(geometry, material);
+    mesh.position.set(0, 0, 0);
+    scene.add(mesh);
 
-//  Direction of Lights
-const directionalLight = new THREE.DirectionalLight(0xFFFFFFF, 0.6);
-scene.add(directionalLight);
+    //  Setting Lights
+    const ambientLight = new THREE.AmbientLight(0xFFFFFF, 0.6);
+    scene.add(ambientLight);
 
-//  Camera Options
-const width = 10;
-const height = width * (window.innerHeight / window.innerWidth);
-const camera = new THREE.OrthographicCamera(
-    width / -2, 
-    width / 2,
-    height / 2,
-    height / -2,
-    1,
-    100
-);
+    //  Direction of Lights
+    const directionalLight = new THREE.DirectionalLight(0xFFFFFFF, 0.6);
+    scene.add(directionalLight);
 
-camera.position.set(4,4,4);
-camera.lookAt(0,0,0);
+    //  Camera Options
+    const width = 10;
+    const height = width * (window.innerHeight / window.innerWidth);
+    const camera = new THREE.OrthographicCamera(
+        width / -2,
+        width / 2,
+        height / 2,
+        height / -2,
+        1,
+        100
+    );
 
-//  Renderer
-const Renderer = new THREE.WebGLRenderer({ antialias: true });
-Renderer.setSize(window.innerWidth, window.innerHeight);
-Renderer.render(scene, camera);
+    camera.position.set(4, 4, 4);
+    camera.lookAt(0, 0, 0);
 
-//  Add to HTML
-document.body.appendChild(Renderer.domElement);
+    //  Renderer
+    const Renderer = new THREE.WebGLRenderer({ antialias: true });
+    Renderer.setSize(window.innerWidth, window.innerHeight);
+    Renderer.render(scene, camera);
+
+    //  Add to HTML
+    document.body.appendChild(Renderer.domElement);
+}
